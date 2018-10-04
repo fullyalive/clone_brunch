65a93eafe2a3dcac872263ca1b6c87d3920c9b69
fullyalive <wogud1202@gmail.com>
2018-10-04 21:24:16 +0900

    forms.py에 summernote 탑재

--- brunch/blog/forms.py
+++ brunch/blog/forms.py
@@ -1,11 +1,15 @@
 from django.forms import ModelForm
 from .models import Post, CustomUser
+from django_summernote.widgets import SummernoteWidget
 
 
 class PostForm(ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'sub_title', 'contents']
+        widgets = {
+            'contents': SummernoteWidget(),
+        }
 
 
 class CustomUserForm(ModelForm):
