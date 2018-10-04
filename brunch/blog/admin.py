91e05f9fad3ef1e39d051f5bd5c4343cd64cc8c4
fullyalive <wogud1202@gmail.com>
2018-10-04 21:18:11 +0900

    summernote admin.py에 적용

--- brunch/blog/admin.py
+++ brunch/blog/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import Post, CustomUser, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Post)
+# Apply summernote to all TextField in model.
+class SomeModelAdmin(SummernoteModelAdmin):  # instead of ModelAdmin
+    summernote_fields = '__all__'
+
+
+admin.site.register(Post, SomeModelAdmin)
 admin.site.register(CustomUser)
 admin.site.register(Comment)
