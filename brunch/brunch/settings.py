92100174a75f266f11c5277f10ad04621f4c74c5
fullyalive <wogud1202@gmail.com>
2018-11-01 21:48:30 +0900

    staticfiles dir 경로 수정

--- brunch/brunch/settings.py
+++ brunch/brunch/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'brunch.urls'
 
-TEMPLATES = [    
+TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
@@ -122,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 AUTH_USER_MODEL = 'blog.CustomUser'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
